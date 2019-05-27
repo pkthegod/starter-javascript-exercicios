4bfec222b9573c0d2e4313f2d3482a0005592795
pkthegod <38172122+pkthegod@users.noreply.github.com>
2019-05-27 16:13:52 -0300

    Personalização da resposta

--- Módulo 01/01.js
+++ Módulo 01/01.js
@@ -1,21 +1,11 @@
 function exibeMensagem(endereco) {
   return (
-    "O usuário mora em " +
-    endereco.cidade +
-    " / " +
-    endereco.uf +
-    ", no bairro " +
-    endereco.bairro +
-    ', na rua "' +
-    endereco.rua +
-    '" com nº ' +
-    endereco.numero +
-    "."
+    `O usuário mora em ${ endereco.cidade } / ${ endereco.uf }, no bairro ${ endereco.bairro }, na rua "${ endereco.rua }" com nº ${ endereco.numero }.`
   );
 }
 
 var endereco = {
-  rua: "Rua dos pinheiros",
+  rua: "Rua dos Pinheiros",
   numero: 1293,
   bairro: "Centro",
   cidade: "São Paulo",
